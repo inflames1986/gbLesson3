908560b4c4bd6058bfc8d983f84dc86d1b50ad03
TMichael <js5859168@gmail.com>
2021-07-02 10:17:35 +0300

    Дз №3

--- src/ru/geekbrains/gbLesson/Main.java
+++ src/ru/geekbrains/gbLesson/Main.java
@@ -1,8 +1,55 @@
 package ru.geekbrains.gbLesson;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
+    static Scanner sc = new Scanner(System.in);
+    static Random random = new Random();
+
     public static void main(String[] args) {
-	// write your code here
+
+        gameLesson3();
+
+    }
+
+    public static void gameLesson3() {
+
+        int rep; // переменная для повтора игры
+
+        do {
+            int rNumber = random.nextInt(9);// генерируем случайное число
+            boolean metka = false;
+
+            for (int i = 0; i < 3; i++) {
+
+                System.out.println("Введите число от 0 до 9");
+                int uNumber = sc.nextInt(); // введенное число пользователя
+
+                if (rNumber > uNumber) {
+                    System.out.println("Загаданное число больше " + uNumber);
+                }
+                if (rNumber < uNumber) {
+                    System.out.println("Загаданное число меньше " + uNumber);
+                }
+                if (rNumber == uNumber) {
+
+                    metka = true;
+                    break;
+                }
+            }
+
+            if (metka) {
+                System.out.println("Загаданное число = " + rNumber + " Вы угадали!");
+            } else {
+                System.out.println("Вы не угадали, загаданное число = " + rNumber + "!");
+
+            }
+            System.out.println("Хотите начать заново? Да - нажмите 1");
+            rep = sc.nextInt();
+        }
+        while (rep == 1);
+        System.out.println("ББ!");
     }
 }
